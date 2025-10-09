3d6386fe79656f516e92fc2aaf9012e04c88b591
jedsmith2004 <jedsmith2004@gmail.com>
2025-10-09 20:21:06 +0100

    Initial setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+}
+
